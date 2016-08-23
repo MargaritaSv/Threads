edf77f7acabff248d564dfa88abddd49eb7f00d2
MargaritaSv <margarita.esti@gmail.com>
2016-08-23 09:31:27 +0300

    threads basic stuff

---
+++ _1MyThread.java
@@ -0,0 +1,14 @@
+package Threads;
+
+/**
+ * Created by magy on 23.08.16.
+ */
+public class _1MyThread extends Thread {
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 30; i++) {
+            System.err.println("Another thread ->"+i);
+
+        }    }
+}
