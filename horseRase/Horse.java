f6c7b53800dfe74557d66bf98cb27aa5837f1e05
MargaritaSv <margarita.esti@gmail.com>
2016-08-23 12:02:27 +0300

    horse rase

---
+++ horseRase/Horse.java
@@ -0,0 +1,16 @@
+package Threads.horseRase;
+
+/**
+ * Created by magy on 23.08.16.
+ */
+public class Horse {
+    private String name;
+    private int metros;
+
+    public Horse(String name, int metros) {
+        this.name = name;
+        this.metros = metros;
+    }
+
+
+}
