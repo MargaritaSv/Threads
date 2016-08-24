e94ecded92b23e16813b7ac23000596a1b94d38e
MargaritaSv <margarita.esti@gmail.com>
2016-08-24 13:41:28 +0300

    main class

--- Main.java
+++ Main.java
@@ -1,34 +1,15 @@
-package Threads;
+package Threads.synchronizeThreads;
 
 /**
- * Created by magy on 23.08.16.
+ * Created by magy on 24.08.16.
  */
 public class Main {
     public static void main(String[] args) {
-        //extends thread
-        Thread thread = new _1MyThread();
+        TreeStore treeStore = new TreeStore();
+        Forest forest = new Forest(treeStore);
+        WoodsMan woodsMan = new WoodsMan(treeStore);
 
-        //implementing runnable
-        Thread thread1 = new Thread(new MyThread());
-
-        thread.start();
-        thread1.start();
-
-
-        //lambda
-        new Thread(() -> System.out.println("thread with lambda")).start();
-
-        //using anonymous class
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Some stuff");
-            }
-        }).start();
-
-
-        for (int i = 0; i < 30; i++) {
-            System.out.println("main thread " + i);
-        }
+        new Thread(forest).start();
+        new Thread(woodsMan).start();
     }
 }
