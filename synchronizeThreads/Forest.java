30087683f58ce823f564e8c74697af382ce7824d
MargaritaSv <margarita.esti@gmail.com>
2016-08-24 12:57:37 +0300

    run method (waiting for spac)

--- synchronizeThreads/Forest.java
+++ synchronizeThreads/Forest.java
@@ -4,7 +4,7 @@ package Threads.synchronizeThreads;
  * Created by magy on 24.08.16.
  */
 public class Forest implements Runnable {
-
+    private static final int NUMBER_OF_TREES = 50;
     private TreeStore treeStore;
 
     public Forest(TreeStore treeStore) {
@@ -13,6 +13,26 @@ public class Forest implements Runnable {
 
     @Override
     public void run() {
+        for (int i = 0; i < NUMBER_OF_TREES; i++) {
 
+            //wait for the space
+            while (!treeStore.hasEnoughSpaceForTree()) {
+                try {
+                    treeStore.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            //produce tree
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            
+        }
     }
 }
