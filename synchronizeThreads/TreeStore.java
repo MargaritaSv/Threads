05ffbd5442c667667bfae55519fbe36ad5d4a39b
MargaritaSv <margarita.esti@gmail.com>
2016-08-24 12:31:56 +0300

    class Tree with method for add tree/s

---
+++ synchronizeThreads/TreeStore.java
@@ -0,0 +1,16 @@
+package Threads.synchronizeThreads;
+
+/**
+ * Created by magy on 24.08.16.
+ */
+public class TreeStore {
+
+    private static final int MAX_THREE = 200000;
+    private int currTree = 0;
+
+    public void addTree(int numberOfTree) {
+        if ((numberOfTree > 0) && (numberOfTree + currTree <= MAX_THREE)) {
+            currTree += numberOfTree;
+        }
+    }
+}
