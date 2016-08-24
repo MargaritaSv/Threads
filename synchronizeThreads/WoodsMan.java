439f1274a16eac28183f0fb697344802d6f03d77
MargaritaSv <margarita.esti@gmail.com>
2016-08-24 13:07:04 +0300

    create class Woodsman with reference to TreesStore class

---
+++ synchronizeThreads/WoodsMan.java
@@ -0,0 +1,18 @@
+package Threads.synchronizeThreads;
+
+/**
+ * Created by magy on 24.08.16.
+ */
+public class WoodsMan implements Runnable {
+
+    private TreeStore treeStore;
+
+    public WoodsMan(TreeStore treeStore) {
+        this.treeStore = treeStore;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
