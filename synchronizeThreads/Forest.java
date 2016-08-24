3a96b18690cc6f22aab3f40cfcc4a808320b5162
MargaritaSv <margarita.esti@gmail.com>
2016-08-24 12:51:10 +0300

    class Forest with refence to TreeStore

---
+++ synchronizeThreads/Forest.java
@@ -0,0 +1,18 @@
+package Threads.synchronizeThreads;
+
+/**
+ * Created by magy on 24.08.16.
+ */
+public class Forest implements Runnable {
+
+    private TreeStore treeStore;
+
+    public Forest(TreeStore treeStore) {
+        this.treeStore = treeStore;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
