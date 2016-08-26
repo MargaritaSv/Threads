94a6dbf890a046cae811144084f2263ce74e6761
MargaritaSv <margarita.esti@gmail.com>
2016-08-26 09:55:49 +0300

    variable for how mush number of exact part we needed

---
+++ car/Car.java
@@ -0,0 +1,14 @@
+package Threads.car;
+
+/**
+ * Created by magy on 26.08.16.
+ */
+public abstract class Car {
+
+    private int number;
+
+    public Car(int number) {
+        this.number = number;
+    }
+
+}
